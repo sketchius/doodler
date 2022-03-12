b7cf845ec85cdafd761b91ae11c8a0cfc9d431e4
Bryce Huhtala <sketchius@gmail.com>
2022-03-12 09:23:27 -0500

    Add fillCell() and drawLine() functions

--- js/script.js
+++ js/script.js
@@ -10,6 +10,9 @@ resetButton.addEventListener("click", () => {
 
 initalizeGrid(16);
 
+let lastX;
+let lastY;
+
 
 
 function initalizeGrid(gridSize) {
@@ -25,17 +28,65 @@ function initalizeGrid(gridSize) {
             let cellDiv = document.createElement("div");
             cellDiv.classList.add("cell");
             cellDiv.classList.add("grid");
+            cellDiv.setAttribute("data-x",rowDiv.childNodes.length ?? 0);
+            cellDiv.setAttribute("data-y",mainContainer.childNodes.length ?? 0);
             cellDiv.setAttribute("data-shade",255);
             cellDiv.addEventListener("mouseenter", function () {
-                //this.classList.add("selected");
-                let shade = this.getAttribute("data-shade");
-                shade = shade / 1.1;
-                this.style.backgroundColor = `rgb(${shade},${shade},${shade})`;
-                this.setAttribute("data-shade",shade);
-                
+                // let shade = this.getAttribute("data-shade");
+                // shade = shade / 1.1;
+                // this.style.backgroundColor = `rgb(${shade},${shade},${shade})`;
+                // this.setAttribute("data-shade",shade);
+                let x = this.getAttribute("data-x");
+                let y = this.getAttribute("data-y");
+                fillCell(x,y);
+                if (lastX != undefined && lastX != 0 && lastY != undefined && lastY != 0) {
+                    console.log(Math.max(Math.abs(x-lastX),Math.abs(y-lastY)));
+                    if (Math.max(Math.abs(x-lastX),Math.abs(y-lastY)) > 1) {
+                        drawLine(+lastX,+lastY,+x,+y);
+                    }
+                }
+                lastX = x;
+                lastY = y;
+                console.log("lastX = " + lastX);
+                console.log("lastY = " + lastY);
             });
             rowDiv.appendChild(cellDiv);
         }
         mainContainer.appendChild(rowDiv);
     }
 }
+
+function fillCell(x,y) {
+    if (y >= mainContainer.childNodes.length) { return; }
+    let rowDiv = mainContainer.childNodes[y];
+    if (rowDiv == undefined) {
+        debugger;
+    }
+    if (x >= rowDiv.childNodes.length) {return; }
+    let cellDiv = rowDiv.childNodes[x];
+    cellDiv.classList.add("selected");
+}
+
+function drawLine(x1,y1,x2,y2) {
+    //Normalize x and y differences so that the larger is 1 or -1
+    let deltaX = x1-x2;
+    let deltaY = y1-y2;
+    let max = Math.max(Math.abs(deltaX),Math.abs(deltaY));
+
+    if (max > 10) {
+        debugger;
+    }
+
+    deltaX /= max;
+    deltaY /= max;
+
+
+    for (let i = 0; i < max; i++) {
+        x2 += deltaX;
+        y2 += deltaY;
+
+        fillCell(Math.round(x2),Math.round(y2));
+    }
+}
+
+
