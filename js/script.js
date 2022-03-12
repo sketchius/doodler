feacdd52741587128524c4da72a76d45e05580d1
Bryce Huhtala <sketchius@gmail.com>
2022-03-11 21:09:58 -0500

    Add reset button with grid size option

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
 const mainContainer = document.querySelector(".main");
+const resetButton = document.querySelector("button");
 
-for (let i = 0; i < 16; i++) {
-    let rowDiv = document.createElement("div");
-    rowDiv.classList.add("row");
-    for (let j = 0; j < 16; j++) {
-        let cellDiv = document.createElement("div");
-        cellDiv.classList.add("cell");
-        cellDiv.textContent = "cell";
-        cellDiv.addEventListener("mouseenter", function() { this.classList.add("selected"); });
-        //cellDiv.addEventListener("mouseleave", function() { this.classList.remove("selected"); });
-        rowDiv.appendChild(cellDiv);
+resetButton.addEventListener("click", () => {
+    let newGridSize = prompt("Please enter grid size:");
+    newGridSize = Math.min(100,newGridSize);
+    initalizeGrid(+newGridSize);
+})
+
+
+initalizeGrid(16);
+
+
+
+function initalizeGrid(gridSize) {
+    let allGridDivs = document.querySelectorAll(".grid");
+
+    allGridDivs.forEach( (gridDiv) => gridDiv.remove());
+
+    for (let i = 0; i < gridSize; i++) {
+        let rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
+        rowDiv.classList.add("grid");
+        for (let j = 0; j < gridSize; j++) {
+            let cellDiv = document.createElement("div");
+            cellDiv.classList.add("cell");
+            cellDiv.classList.add("grid");
+            // cellDiv.textContent = "cell";
+            cellDiv.addEventListener("mouseenter", function () { this.classList.add("selected"); });
+            //cellDiv.addEventListener("mouseleave", function() { this.classList.remove("selected"); });
+            rowDiv.appendChild(cellDiv);
+        }
+        mainContainer.appendChild(rowDiv);
     }
-    mainContainer.appendChild(rowDiv);
 }
